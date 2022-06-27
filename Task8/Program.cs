fedfaae4d761e5d49600a77f3c4faea033d4fcd7
Daxett <Daxett@mail.ru>
2022-06-27 22:13:56 +0300

    проверка

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N. (не включая N)
+
+Console.WriteLine ("ВВедите число и вы увидите все четные числа до этого числа ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < n; i++)
+{
+    if (i % 2 == 0)
+        Console.WriteLine(i);
+}
