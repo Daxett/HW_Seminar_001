9f265d25cfbf7bcd8819f3e917ac51610bf3525b
Daxett <Daxett@mail.ru>
2022-06-27 22:05:36 +0300

    add Task2, Task4, task6

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+// Числа вводятся в ручную 
+
+Console.WriteLine ("ВВедите 2  любых целых числа ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) 
+{
+    Console.WriteLine("max = {0}", a);
+}
+else
+{
+    Console.WriteLine("max = {0}", b);
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// чиста вводятся в ручную
+
+Console.WriteLine ("ВВедите 3  любых целых числа ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+    if (a > c)
+        Console.WriteLine("max = {0}", a);
+    else
+        Console.WriteLine("max = {0}", c);
+else
+    if (b > c)
+        Console.WriteLine("max = {0}", b);
+    else
+        Console.WriteLine("max = {0}", c);
